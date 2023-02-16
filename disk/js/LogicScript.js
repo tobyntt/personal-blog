8098c086882224f3ede2545a3984f94c64773878
tobyntt <tobyntt.nguyen@gmail.com>
2023-02-16 20:21:55 +1100

    finished building up header and footer for article page

--- disk/js/LogicScript.js
+++ disk/js/LogicScript.js
@@ -4,24 +4,36 @@ const body = document.getElementById("body-home");
 const navLinks = document.getElementById("navLinks");
 const burgerButton = document.getElementById("burgerButton");
 const toggleBurgerButton = () => {
-  /* clear the body's class */
-  body.classList.remove("disable-vertical-scrollbar");
+  /* re-activate the verticle scroll bar */
+  body.style.overflowY = "scroll";
   /* toggle navbar */
   navLinks.classList.toggle("navbar-links-open");
   /* toggle burger menu */
   burgerButton.classList.toggle("navbar-burger-button-open");
 };
 
-/* click outside to close the navbar menu on table or mobile screens and disable the vertical scroll bar when the mobile menu is open */
+/* click outside to close the navbar menu on table or mobile screens */
+/* also disable the vertical scroll bar when the mobile menu is open */
 document.onclick = (e) => {
   if (e.target.id !== "navLinks" && e.target.id !== "burgerButton") {
-    /* clear the body's class */
-    body.classList.remove("disable-vertical-scrollbar");
+    /* re-activate the verticle scroll bar */
+    body.style.overflowY = "scroll";
     navLinks.classList.remove("navbar-links-open");
     burgerButton.classList.remove("navbar-burger-button-open");
   }
 
   if (navLinks.classList.contains("navbar-links-open")) {
-    body.classList.add("disable-vertical-scrollbar");
+    /* disable the verticle scroll bar */
+    body.style.overflowY = "hidden";
   }
 };
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+/* ========================= ARTICLE PAGE ========================= */
+/* use flex-box order to re-arrange hero element: title, intro, image */
+const heroContainer = document.getElementById("hero-container");
+if (heroContainer.classList.contains("hero-article-container")) {
+  heroContainer.childNodes[5].style.order = "1";
+  heroContainer.childNodes[1].style.order = "5";
+}
